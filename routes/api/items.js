440c15c7b06f2ac199c1b5e625cfb793dba35085
sarikadara <65969202+sarikadara@users.noreply.github.com>
2020-07-08 12:19:13 +0530

    navbar

--- routes/api/items.js
+++ routes/api/items.js
@@ -12,8 +12,36 @@ const item = require('../../models/item');
 router.get('/', (req, res) =>{
     item.find()
     .sort({ date: -1 })
-     .then(items => res.json(items))
+     .then(items => res.json(items));
 });
+//Route get post api/items
+//@desc create post
+//@access public
+
+router.post('/', (req, res) =>{
+    const newItem = newItem({
+        name: req.body.name
+    });
+    newItem.save().then(item => res.json(item));
+});
+// Route delete api/items
+//@desc create an item
+//@access public
+
+router
+ 
+    .delete('/:id',  (req, res) =>{
+        item.findById(req.params.id).then(item =>
+         item.remove().then(() => res.json({success: true}))
+        )
+        .catch(err => res.status(404).json({success: false}));
+    
+     })
+    
+
+
+
+
 
      
     
