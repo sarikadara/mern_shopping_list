6c7121497e169d14fb6765dfa0c7fcf50e8b74b8
sarikadara <65969202+sarikadara@users.noreply.github.com>
2020-11-05 15:27:05 +0530

    VBNM

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import React, {Component}from 'react';
+import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
-import ItemModal from './components/ItemModal'; 
+import ItemModal from './components/ItemModal';
 import { Container } from 'reactstrap';
 
 import { Provider } from 'react-redux';
@@ -12,19 +12,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 
-class  App extends Component {
+class App extends Component {
   componentDidMount() {
     store.dispatch(loadUser());
   }
   render() {
     return (
       <Provider store={store}>
-       <div className="App">
-         <AppNavbar />
-         <Container>
-           <ItemModal />
-           <ShoppingList />
-         </Container>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
         </div>
       </Provider>
     );
